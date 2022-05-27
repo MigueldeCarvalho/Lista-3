28542d3c8895de687a053c1c60bb057b9682e78c
MigueldeCarvalho <103599041+MigueldeCarvalho@users.noreply.github.com>
2022-05-27 18:33:15 -0300

    Add files via upload

---
+++ ex4.php
@@ -0,0 +1,20 @@
+<?php
+$Veiculo = $_POST['Veiculo'];
+echo "Nome do veiculo informado: $Veiculo";
+echo "<br/>";
+
+$Modelo = $_POST['Modelo'];
+echo "Modelo de carro informado = $Modelo";
+echo "<br/>";
+
+$Valor = (int) $_POST['Valor'];
+echo "Valor do carro informado = $Valor";
+$um = $Valor/100;
+$ipi =  $um*9;
+echo "<br/>";
+echo "Valor da taxa de ipi é: $ipi";
+$icms = $um*17;
+echo "<br/>";
+echo "Valor da taxa de icms é: $icms";
+echo "<br/>";
+?>
---
+++ ex5.php
@@ -0,0 +1,17 @@
+<?php
+    // taxa de juros
+    $taxa = 9.48;
+
+    $montante = $_GET['valorMontante'];
+    $anos = $_GET['anos'];
+
+    $montante *= $taxa * $anos;
+    $cadaParcela = $montante / ($anos * 12);
+
+    echo "Montante: $montante <br>";
+    echo "Valor de cada parcela: $cadaParcela <br><br>";
+    echo "Taxa: $taxa <br>";
+    echo "Tempo: $anos anos <br>";
+?>
+
+<a href="ex5.html">Voltar ao Calcular</a>
